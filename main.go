9c630663459a00d71f4fdb66bf65078d17a030dc
Brendan Ashton <brendan.ashton@getweave.com>
2022-09-19 13:53:09 -0600

    WIP: copy files

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -12,28 +14,87 @@ import (
 )
 
 const (
-	targetVideoFilesPath = "/Users/brendanashton/Desktop/small but mighty"
-	sourceVideoFilesPath = "/Volumes/Untitled/DCIM/100MEDIA"
+	// targetVideoFilesPath = "/Users/brendanashton/Desktop/small but mighty"
+	targetVideoFilesPath = "/Users/brendan.ashton/tmp/tmp2"
+	//sourceVideoFilesPath = "/Volumes/Untitled/DCIM/100MEDIA"
+	sourceVideoFilesPath = "/Users/brendan.ashton/tmp"
 )
+
+type copyFunc func(string, string) (int64, error)
 
 func main() {
 	d, err := os.ReadDir(sourceVideoFilesPath)
 	if err != nil {
 		log.Fatalf("could not read directory: %v", err)
 	}
+	if len(d) == 0 {
+		fmt.Println("no files found in directory: " + sourceVideoFilesPath)
+		return
+	}
 
+	CopyFiles(d, targetVideoFilesPath, copy)
+	RenameFiles(d)
+}
+
+func copy(src, dst string) (int64, error) {
+	if src == "" || dst == "" {
+		return 0, fmt.Errorf("src and dst are both required")
+	}
+
+	// if it starts with ".", ignore it.
+	if string(filepath.Base(src)[0]) == "." {
+		return 0, nil
+	}
+
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
+
+func CopyFiles(d []fs.DirEntry, targetPath string, copyFunc copyFunc) {
+	if len(d) == 0 || targetPath == "" {
+		fmt.Println("directory must not be empty and target path must be specified")
+		return
+	}
+
+	for _, f := range d {
+		_, err := copyFunc(f.Name(), path.Join(targetPath, f.Name()))
+		if err != nil {
+			log.Fatalf("error copying file: %v", err)
+		}
+	}
+}
+
+func RenameFiles(d []fs.DirEntry) {
 	for _, v := range d {
 		info, _ := v.Info()
 		if v.Type().IsRegular() && strings.Contains(v.Name(), "DJI_0") && !strings.Contains(info.Name(), strconv.Itoa(time.Now().Year())) {
 			fmt.Println("original name: " + v.Name())
 			fmt.Println("name to rename to: " + attachTimeToFileName(targetVideoFilesPath, info))
-			err := os.Rename(sourceVideoFilesPath+ "/" + v.Name(), attachTimeToFileName(sourceVideoFilesPath, info))
+			err := os.Rename(sourceVideoFilesPath+"/"+v.Name(), attachTimeToFileName(sourceVideoFilesPath, info))
 			if err != nil {
 				log.Fatalf("error renaming file: %v", err)
-			}	
+			}
 		}
-		
-		// fmt.Printf("name: '%s' type: '%v' info: '%v'\n", v.Name(), v.Type(), info.ModTime())
 	}
 }
 
